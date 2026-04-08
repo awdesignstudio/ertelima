ad097d2d43e62e465e7468d4d5676c72d75bad5f
awdesignstudio <end.joy.urself@gmail.com>
2026-04-08 18:04:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rt05-portal-v2.9'; // Ganti tiap ada update besar
+const CACHE_NAME = 'rt05-portal-v3.0'; // Ganti tiap ada update besar
 
 // 1. Sesuaikan file yang di-cache dengan struktur repository Mas Adhi
 const urlsToCache = [
